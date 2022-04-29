93cab1a8efc0c6c3eb9d5322c67ce7e318975dec
Ayikanying <Ayikanying@users.noreply.github.com>
2022-04-30 02:04:04 +0300

    designed the frontend of the application

--- jobapp/models.py
+++ jobapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Jobs(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date_posted = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
